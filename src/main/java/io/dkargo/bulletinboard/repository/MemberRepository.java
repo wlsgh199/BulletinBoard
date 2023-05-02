30d2e1c60b925d8ca430b306e1edcda2918e6a8d
jhpark <wlsgh950221@gmail.com>
2023-05-02 19:06:46 +0900

    파일 이동으로 인해 삭제

--- src/main/java/io/dkargo/bulletinboard/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package io.dkargo.bulletinboard.repository;
-
-import io.dkargo.bulletinboard.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
