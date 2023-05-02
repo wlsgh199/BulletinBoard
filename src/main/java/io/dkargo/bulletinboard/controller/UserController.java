c4976839b686716b088ee5771250298c4c41b61f
jhpark <wlsgh950221@gmail.com>
2023-05-03 01:07:01 +0900

    test code 샘플 추가

---
+++ src/main/java/io/dkargo/bulletinboard/controller/UserController.java
@@ -0,0 +1,31 @@
+package io.dkargo.bulletinboard.controller;
+
+import io.dkargo.bulletinboard.dto.request.ReqUserDTO;
+import io.dkargo.bulletinboard.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("/users")
+@Transactional
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @ApiOperation(value = "회원 추가")
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public void saveMember(
+            @ApiParam(value = "유저 이름", required = true) @RequestBody ReqUserDTO reqUserDTO) {
+        userService.saveMember(reqUserDTO);
+    }
+}
