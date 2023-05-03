f5d2fd8c6114c91628116d680ba10f4b8bca51bd
jhpark <jhpark@dkargo.io>
2023-05-03 18:49:51 +0900

    테스트 코드 작성

--- src/main/java/io/dkargo/bulletinboard/dto/request/post/ReqGetDTO.java
+++ src/main/java/io/dkargo/bulletinboard/dto/request/post/ReqGetDTO.java
@@ -1,5 +1,6 @@
 package io.dkargo.bulletinboard.dto.request.post;
 
+import com.sun.istack.NotNull;
 import io.dkargo.bulletinboard.dto.common.OrderByListEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -11,18 +12,21 @@ import lombok.Setter;
 @Setter
 public class ReqGetDTO {
     @ApiModelProperty(value = "유저 아이디")
+    @NotNull
     private Long userId;
+
     @ApiModelProperty(value = "카테고리 아이디")
     private Long categoryId;
     @ApiModelProperty(value = "게시물 제목")
     private String title;
+
     @ApiModelProperty(value = "게시물 내용")
     private String content;
 
-    @ApiModelProperty(value = "페이징 offset" , example = "0")
-    private Long page;
+    @ApiModelProperty(value = "페이징 offset", example = "0")
+    private int page;
     @ApiModelProperty(value = "페이징 사이즈", example = "10")
-    private Long size;
+    private int size;
     @ApiModelProperty(value = "정렬 기준", required = true)
     private OrderByListEnum orderByListEnum;
 
---
+++ src/test/java/io/dkargo/bulletinboard/PostServiceTest.java
@@ -0,0 +1,75 @@
+package io.dkargo.bulletinboard;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dkargo.bulletinboard.dto.common.OrderByListEnum;
+import io.dkargo.bulletinboard.dto.request.category.ReqAddCategoryDTO;
+import io.dkargo.bulletinboard.dto.request.post.ReqAddPostDTO;
+import io.dkargo.bulletinboard.dto.request.post.ReqGetDTO;
+import io.dkargo.bulletinboard.entity.Category;
+import io.dkargo.bulletinboard.entity.Post;
+import io.dkargo.bulletinboard.repository.PostRepository;
+import io.dkargo.bulletinboard.service.CategoryService;
+import io.dkargo.bulletinboard.service.PostService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("게시물 전체 조회 테스트")
+    public void findPostByReqGetDTO() {
+        ReqGetDTO reqGetDTO = new ReqGetDTO();
+        reqGetDTO.setOrderByListEnum(OrderByListEnum.ORDER_BY_POST_ID_DESC);
+        reqGetDTO.setPage(0);
+        reqGetDTO.setSize(1);
+
+        postService.findPostByReqGetDTO(reqGetDTO);
+    }
+
+    @Test
+    @DisplayName("게시물 저장 테스트")
+    public void addPost() throws IOException {
+
+        ReqAddPostDTO reqAddPostDTO = new ReqAddPostDTO();
+        reqAddPostDTO.setTitle("test title");
+        reqAddPostDTO.setContent("test content");
+        reqAddPostDTO.setCategoryId(4L);
+        reqAddPostDTO.setUserId(1L);
+        reqAddPostDTO.setPostOpenUseFlag("Y");
+        reqAddPostDTO.setReplyCommentUseFlag("Y");
+        reqAddPostDTO.setPostPassword("1234");
+
+        postService.addPost(reqAddPostDTO);
+
+        //addPost함수의 리턴이 없으니 게시물 전체 조회해서 가장 마지막에있는것을 가져온다.
+        List<Post> postList = postRepository.findAll();
+        Post post = postList.get(postList.size() - 1);
+
+        //내가 저장한 게시물 맞는지 확인
+        Assertions.assertEquals(reqAddPostDTO.getTitle(), post.getTitle());
+        Assertions.assertEquals(reqAddPostDTO.getContent(), post.getContent());
+        Assertions.assertEquals(reqAddPostDTO.getUserId(), post.getUser().getId());
+        Assertions.assertEquals(reqAddPostDTO.getReplyCommentUseFlag(), post.getReplyCommentUseFlag());
+        Assertions.assertEquals(reqAddPostDTO.getPostPassword(), post.getPostPassword());
+        Assertions.assertEquals(reqAddPostDTO.getPostOpenUseFlag(), post.getPostOpenUseFlag());
+    }
+
+}
